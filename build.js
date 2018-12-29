4a749ce9364bba262bac3904fd2bd917731e3232
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-12-29 20:10:54 +0530

    Add build.js

--- build.js
+++ build.js
@@ -1,104 +1,55 @@
 'use strict'
 
-// Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require('chalk');
+const boxen = require('boxen');
+const fs = require('fs');
+const path = require('path');
+const newline = '\n';
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round'
-}
+};
 
 const info = {
 	name: "Ayesh Karunaratne",
-	work: "Freelance Software Engineer",
-	opensource: "PHP, JS and Infosec",
-	twitter: "Ayeshlive",
-	github: "Ayesh",
+	work: "PHP, JS, Databases, Performance and Security",
 	web: "https://ayesh.me",
-	pgp: "0x2CA18B2097A7429A",
-	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
 	contact: "https://ayesh.me/contact",
-	npx: "ayesh"
-}
-
-const card_fields = {
-	name: chalk.white('               Ayesh Karunaratne'),
-	work: chalk.white('Freelance Software Engineer'),
-	opensource: "PHP, JS and Infosec",
-	twitter: "Ayeshlive",
 	github: "Ayesh",
-	web: "https://ayesh.me",
-	pgp: "0x2CA18B2097A7429A",
-	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
-	contact: "https://ayesh.me/contact",
-	npx: "ayesh"
-}
-
-const card = {
-	name: chalk.white('               Ayesh Karunaratne'),
-	work: "Freelance Software Engineer",
-	opensource: "PHP, JS and Infosec",
-	twitter: "Ayeshlive",
-	github: "Ayesh",
-	web: "https://ayesh.me",
-	pgp: "0x2CA18B2097A7429A",
-	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
-	contact: "https://ayesh.me/contact",
 	drupal: "Ayesh",
-	npx: "ayesh"
-}
+	twitter: "Ayeshlive",
+	npm: "ayesh",
+	gpg: "0x2CA18B2097A7429A",
+	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Ayesh Karunaratne'),
-  work: chalk.white('Freelance Software Engineer'),
-  opensource: chalk.gray('PHP, JS, Databases, Performance and Security'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('ayeshlive'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~ayesh'),
-  github: chalk.gray('https://github.com/') + chalk.green('Ayesh'),
-  drupal: chalk.gray('https://drupal.org/u/') + chalk.cyan('Ayesh'),
-  web: chalk.cyan('https://ayesh.me'),
-  npx: chalk.red('npx') + ' ' + chalk.white('ayesh'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:'),
-  labelDrupal: chalk.white.bold('     Drupal:')
-}
+  name: chalk.white.bold('             ' + info.name),
+  work: chalk.gray(info.work),
+  web: chalk.white.bold(    '        Web:  ') + chalk.cyan(info.web),
+  contact: chalk.white.bold('    Contact:  ') + chalk.cyan(info.contact),
+  github: chalk.white.bold( '     GitHub:  ') + chalk.gray('https://github.com/') + chalk.green(info.github),
+  drupal: chalk.white.bold( '     Drupal:  ') + chalk.gray('https://drupal.org/u/') + chalk.cyan(info.drupal),
+  twitter: chalk.white.bold('    Twitter:  ') + chalk.gray('https://twitter.com/') + chalk.cyan(info.twitter),
+  npm: chalk.white.bold(    '        npm:  ') + chalk.gray('https://npmjs.com/') + chalk.red('~' + info.npm),
+  gpg: chalk.white.bold(    '    GPG Key:  ') + chalk.gray(info.gpg),
+  card: chalk.white.bold(   '       Card:  ') + chalk.red('npx ' + info.npm)
+};
 
-// Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name}`
-const working = `           ${data.work}`
-const opensourcing = `  ${data.opensource}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const webing = `${data.labelWeb}  ${data.web}`
-const drupaling = `${data.labelDrupal}  ${data.drupal}`
-const carding = `${data.labelCard}  ${data.npx}`
-
-// Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+const output = data.name +
+               newline + newline +
+               data.work + newline + newline +
 			   
-			   webing + newline + // data.labelWeb + data.web
-			   githubing + newline + // data.labelGitHub + data.github
-			   drupaling + newline + // data.labelGitHub + data.github
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + newline + // data.labelnpm + data.npm
-               
+			   data.web + newline +
+			   data.contact + newline +
+			   data.github + newline +
+			   data.drupal + newline +
+			   data.twitter + newline +
+			   data.gpg + newline + newline +
+               data.card
 
-               carding // data.labelCard + data.npx
-
-fs.writeFileSync(path.join(__dirname, 'output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'output'), chalk.green(boxen(output, options)));
