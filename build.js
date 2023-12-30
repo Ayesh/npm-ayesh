ca5b7958edaa34e72c27b024e8b56655f6dac59d
Ayesh Karunaratne <ayesh@aye.sh>
2023-12-31 01:01:50 +0700

    Update info

--- build.js
+++ build.js
@@ -15,13 +15,10 @@ const options = {
 
 const info = {
 	name: "Ayesh Karunaratne",
-	work: "PHP, JS, Databases, Performance and Security",
-	web: "https://ayesh.me",
+	work: "PHP, Security, Databases, Performance, Architecture",
+	web: "https://aye.sh",
 	contact: "https://ayesh.me/contact",
 	github: "Ayesh",
-	drupal: "Ayesh",
-	twitter: "Ayeshlive",
-	npm: "ayesh",
 	gpg: "0x2CA18B2097A7429A",
 	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
 };
@@ -33,9 +30,6 @@ const data = {
   web: chalk.white.bold(    '        Web:  ') + chalk.cyan(info.web),
   contact: chalk.white.bold('    Contact:  ') + chalk.cyan(info.contact),
   github: chalk.white.bold( '     GitHub:  ') + chalk.gray('https://github.com/') + chalk.green(info.github),
-  drupal: chalk.white.bold( '     Drupal:  ') + chalk.gray('https://drupal.org/u/') + chalk.cyan(info.drupal),
-  twitter: chalk.white.bold('    Twitter:  ') + chalk.gray('https://twitter.com/') + chalk.cyan(info.twitter),
-  npm: chalk.white.bold(    '        npm:  ') + chalk.gray('https://npmjs.com/') + chalk.red('~' + info.npm),
   gpg: chalk.white.bold(    '    GPG Key:  ') + chalk.gray(info.gpg),
   card: chalk.white.bold(   '       Card:  ') + chalk.red('npx ' + info.npm)
 };
@@ -47,8 +41,6 @@ const output = data.name +
 			   data.web + newline +
 			   data.contact + newline +
 			   data.github + newline +
-			   data.drupal + newline +
-			   data.twitter + newline +
 			   data.gpg + newline + newline +
                data.card
 
