a564726e85024362c603f5c4ae7b95c32b7882a1
Ayesh Karunaratne <ayesh@aye.sh>
2026-01-28 08:50:49 +0700

    Code lint with standard

--- build.js
+++ build.js
@@ -1,47 +1,47 @@
 'use strict'
 
-const chalk = require('chalk');
-const boxen = require('boxen');
-const fs = require('fs');
-const path = require('path');
-const newline = '\n';
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+const newline = '\n'
 
 // Define options for Boxen
 const info = {
-	name: "Ayesh Karunaratne",
-	work: "PHP, Security, Databases, Performance, Architecture",
-	web: "https://aye.sh",
-	contact: "https://ayesh.me/contact",
-	github: "Ayesh",
-	npm: 'ayesh',
-	gpg: "0x2CA18B2097A7429A",
-	ssh: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9",
-};
+  name: 'Ayesh Karunaratne',
+  work: 'PHP, Security, Databases, Performance, Architecture',
+  web: 'https://aye.sh',
+  contact: 'https://ayesh.me/contact',
+  github: 'Ayesh',
+  npm: 'ayesh',
+  gpg: '0x2CA18B2097A7429A',
+  ssh: 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUYHEA1oSAnwA0dT1QhJxorqpmxX6ENjAznICIE5yZ9'
+}
 
 const options = {
-	padding: 1,
-	margin: 1,
-	borderStyle: 'double',
-	title: info.name,
-	titleAlignment: 'center'
+  padding: 1,
+  margin: 1,
+  borderStyle: 'double',
+  title: info.name,
+  titleAlignment: 'center'
 }
 
 // Text + chalk definitions
 const data = {
   work: chalk.gray(info.work),
-  web: chalk.white.bold(    '        Web:  ') + chalk.cyan(info.web),
+  web: chalk.white.bold('        Web:  ') + chalk.cyan(info.web),
   contact: chalk.white.bold('    Contact:  ') + chalk.cyan(info.contact),
-  github: chalk.white.bold( '     GitHub:  ') + chalk.gray('https://github.com/') + chalk.green(info.github),
-  gpg: chalk.white.bold(    '    GPG Key:  ') + chalk.gray(info.gpg),
-  card: chalk.white.bold(   '       Card:  ') + chalk.red('npx ' + info.npm)
-};
+  github: chalk.white.bold('     GitHub:  ') + chalk.gray('https://github.com/') + chalk.green(info.github),
+  gpg: chalk.white.bold('    GPG Key:  ') + chalk.gray(info.gpg),
+  card: chalk.white.bold('       Card:  ') + chalk.red('npx ' + info.npm)
+}
 
 const output = data.work + newline +
-			   
+
 			   data.web + newline +
 			   data.contact + newline +
 			   data.github + newline +
 			   data.gpg + newline + newline +
                data.card
 
-fs.writeFileSync(path.join(__dirname, 'output'), chalk.green(boxen(output, options)));
+fs.writeFileSync(path.join(__dirname, 'output'), chalk.green(boxen(output, options)))
